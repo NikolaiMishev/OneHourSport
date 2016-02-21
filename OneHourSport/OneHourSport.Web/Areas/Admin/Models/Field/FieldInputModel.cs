e256fd8baae20612384780919d5fba2bac2f56d2
Nikolai <nikolaimishev@gmail.com>
2016-02-21 18:18:31 +0200

    Management of fields added to admin area!

---
+++ OneHourSport/OneHourSport.Web/Areas/Admin/Models/Field/FieldInputModel.cs
@@ -0,0 +1,28 @@
+﻿namespace OneHourSport.Web.Areas.Admin.Models.Field
+{
+    using System.ComponentModel.DataAnnotations;
+
+    using Common.Constants;
+    
+    public class FieldInputModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 5)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(1000, MinimumLength = 50)]
+        public string Description { get; set; }
+
+        [Required]
+        public decimal PricePerHour { get; set; }
+        
+        public bool isAprooved { get; set; }
+
+        public bool isCovered { get; set; }
+
+        public SportCategory Category { get; set; }
+    }
+}
---
+++ OneHourSport/OneHourSport.Web/Areas/Admin/Models/Field/FieldViewModel.cs
@@ -0,0 +1,31 @@
+﻿namespace OneHourSport.Web.Areas.Admin.Models.Field
+{
+    using System;
+    using AutoMapper;
+    using OneHourSport.Common.Constants;
+    using OneHourSport.Models;
+    using OneHourSport.Web.Infrastructure;
+
+    public class FieldViewModel : IMapFrom<SportField>, IHaveCustomMappings
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public decimal PricePerHour { get; set; }
+
+        public bool isAprooved { get; set; }
+
+        public bool isCovered { get; set; }
+
+        public SportCategory Category { get; set; }
+
+        public void CreateMappings(IConfiguration configuration)
+        {
+            configuration.CreateMap<SportField, FieldViewModel>("FieldViewModel")
+                .ForMember(r => r.Description, opt => opt.MapFrom(r => r.Description.Length > 50 ? r.Description.Substring(0, 50)+"..." : r.Description));
+        }
+    }
+}
