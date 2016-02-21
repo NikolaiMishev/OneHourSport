f10c952ac4b362375ead4bc9fdfea71dfb70a1ba
Nikolai <nikolaimishev@gmail.com>
2016-02-21 19:11:50 +0200

    Management of Complexes added to admin area and also global area fix!

---
+++ OneHourSport/OneHourSport.Web/Areas/Admin/Controllers/ComplexAdminController.cs
@@ -0,0 +1,79 @@
+﻿namespace OneHourSport.Web.Areas.Admin.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Data;
+    using System.Data.Entity;
+    using System.Linq;
+    using System.Net;
+    using System.Web;
+    using System.Web.Mvc;
+    using Kendo.Mvc.Extensions;
+    using Kendo.Mvc.UI;
+    using OneHourSport.Models;
+    using Data.Repositories;
+    using Models.Complex;
+    using AutoMapper.QueryableExtensions;
+    public class ComplexAdminController : Controller
+    {
+        private IRepository<SportComplex> complexes;
+
+        public ComplexAdminController(IRepository<SportComplex> complexes)
+        {
+            this.complexes = complexes;
+        }
+
+        public ActionResult AdminComplex()
+        {
+            return View();
+        }
+
+        public ActionResult SportComplexes_Read([DataSourceRequest]DataSourceRequest request)
+        {
+            var result = this.complexes
+                 .All()
+                 .ProjectTo<ComplexViewModel>()
+                 .ToList()
+                 .ToDataSourceResult(request);
+
+            return Json(result);
+        }
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult SportComplexes_Update([DataSourceRequest]DataSourceRequest request, ComplexInputModel sportComplex)
+        {
+            if (ModelState.IsValid)
+            {
+                var entity = this.complexes.GetById(sportComplex.Id);
+                entity.Name = sportComplex.Name;
+                entity.Description = sportComplex.Description;
+                entity.WorkHourFrom = sportComplex.WorkHourFrom;
+                entity.WorkHourTo = sportComplex.WorkHourTo;
+                entity.Address = sportComplex.Address;
+                
+
+                this.complexes.SaveChanges();
+            }
+            var postToDisplay = this.complexes.All()
+                           .ProjectTo<ComplexViewModel>()
+                           .FirstOrDefault(x => x.Id == sportComplex.Id);
+
+            return Json(new[] { sportComplex }.ToDataSourceResult(request, ModelState));
+        }
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult SportComplexes_Destroy([DataSourceRequest]DataSourceRequest request, SportComplex sportComplex)
+        {
+            this.complexes.Delete(sportComplex.Id);
+            this.complexes.SaveChanges();
+
+            return Json(new[] { sportComplex }.ToDataSourceResult(request, ModelState));
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            this.complexes.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ OneHourSport/OneHourSport.Web/Areas/Admin/Models/Complex/ComplexViewModel.cs
@@ -0,0 +1,30 @@
+﻿namespace OneHourSport.Web.Areas.Admin.Models.Complex
+{
+    using Infrastructure;
+    using OneHourSport.Models;
+    using AutoMapper;
+
+    public class ComplexViewModel : IMapFrom<SportComplex>, IHaveCustomMappings
+    {
+        public int Id { get; set; }
+        
+        public string Name { get; set; }
+        
+        public string Description { get; set; }
+        
+        public int WorkHourFrom { get; set; }
+        
+        public int WorkHourTo { get; set; }
+                
+        public string Address { get; set; }
+
+        public string CityName { get; set; }
+
+        public void CreateMappings(IConfiguration configuration)
+        {
+            configuration.CreateMap<SportComplex, ComplexViewModel>("ComplexViewModel")
+               .ForMember(r => r.Description, opt => opt.MapFrom(r => r.Description.Length > 50 ? r.Description.Substring(0, 50) + "..." : r.Description))
+               .ForMember(r => r.CityName, opt => opt.MapFrom(r => r.City.Name));
+        }
+    }
+}
