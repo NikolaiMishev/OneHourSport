e725e3d4a0aa658024fc325ecab3e2b35658c558
Nikolai <nikolaimishev@gmail.com>
2016-02-23 22:28:04 +0200

    Missed to add tests!

--- OneHourSport/OneHourSport.Tests/RoutesTests/FieldRoutesTests.cs
+++ OneHourSport/OneHourSport.Tests/RoutesTests/FieldRoutesTests.cs
@@ -1,16 +1,19 @@
 ﻿namespace OneHourSport.Tests.RoutesTests
 {
     using System.Web.Routing;
+
     using MvcRouteTester;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
     using Web.Controllers;
     using Web;
 
+    using Common.Constants;
+
     [TestClass]
     public class FieldRoutesTests
     {
         [TestMethod]
-        public void TestRouteById()
+        public void TestRouteFieldDetailsById()
         {
             var routeCollection = new RouteCollection();
             RouteConfig.RegisterRoutes(routeCollection);
@@ -22,7 +25,7 @@
         }
 
         [TestMethod]
-        public void TestRouteByInvalidId()
+        public void TestRouteFieldDetailsByInvalidId()
         {
             var routeCollection = new RouteCollection();
             RouteConfig.RegisterRoutes(routeCollection);
@@ -43,6 +46,54 @@
             routeCollection
                 .ShouldMap(urlForTest)
                 .To<FieldController>(x => x.FieldDetails(1, true));
+        }
+
+        [TestMethod]
+        public void TestRouteListAllByCategory()
+        {
+            var routeCollection = new RouteCollection();
+            RouteConfig.RegisterRoutes(routeCollection);
+            var urlForTest = "/Field/ListFieldsByCategory/FootBall?page=1";
+
+            routeCollection
+                .ShouldMap(urlForTest)
+                .To<FieldController>(x => x.ListFieldsByCategory(SportCategory.FootBall, 1));
+        }
+
+        [TestMethod]
+        public void TestRouteEditField()
+        {
+            var routeCollection = new RouteCollection();
+            RouteConfig.RegisterRoutes(routeCollection);
+            var urlForTest = "/Field/EditField?fieldId=1";
+
+            routeCollection
+                .ShouldMap(urlForTest)
+                .To<FieldController>(x => x.EditField(1));
+        }
+
+        [TestMethod]
+        public void TestRouteDestryField()
+        {
+            var routeCollection = new RouteCollection();
+            RouteConfig.RegisterRoutes(routeCollection);
+            var urlForTest = "/Field/Destroy?fieldId=1";
+
+            routeCollection
+                .ShouldMap(urlForTest)
+                .To<FieldController>(x => x.Destroy(1));
+        }
+
+        [TestMethod]
+        public void TestRouteCreateFieldByComplexId()
+        {
+            var routeCollection = new RouteCollection();
+            RouteConfig.RegisterRoutes(routeCollection);
+            var urlForTest = "/Field/Create?complexId=1";
+
+            routeCollection
+                .ShouldMap(urlForTest)
+                .To<FieldController>(x => x.Create(1));
         }
     }
 }
